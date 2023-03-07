ce94f8c8605de1927eced954698f5034f4400ed1
shubham <shubham.bhoyar077@gmail.com>
2023-03-07 19:40:19 +0530

    Update addScore module with fetch api

--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -1,8 +1,14 @@
-import renderScore from './renderScore.js';
-
-const addScore = (name, score) => {
-  // scoreObj = ;
-  renderScore({ name, score });
+const addScore = async (name, score) => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/5SLb2yuqigv9cgRo1gPU/scores/',{
+    method:"POST",
+    body: JSON.stringify({
+      user:name,
+      score:score
+    }),
+    headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+  });
 };
 
 export default addScore;
