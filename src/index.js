1e43fb4ff03a32839dc8a3ebda9b5fe3a6ed1b3e
shubham <shubham.bhoyar077@gmail.com>
2023-03-07 15:41:24 +0530

    Add logic for render score dynamically

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import "./css/style.css"
+import renderScore from "./modules/renderScore.js";
+
+const playerScore = [
+  {
+    name: "Example1",
+    score: "100"
+  },
+  {
+    name: "Example2",
+    score: "20"
+  },
+  {
+    name: "Example3",
+    score: "50"
+  },
+  {
+    name: "Example4",
+    score: "10"
+  },
+  {
+    name: "Example5",
+    score: "60"
+  },
+  {
+    name: "Example6",
+    score: "30"
+  },
+]
+
+playerScore.forEach( element => renderScore(element));
