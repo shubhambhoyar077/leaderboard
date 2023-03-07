ff526e7977233ade91bfb091b7da3da9cadf74e6
shubham <shubham.bhoyar077@gmail.com>
2023-03-07 20:12:43 +0530

    Add game id

--- src/index.js
+++ src/index.js
@@ -2,25 +2,16 @@ import './css/style.css';
 import addScore from './modules/addScore.js';
 import refreshScores from './modules/getScores.js';
 
+const TESTGAMEID = "5SLb2yuqigv9cgRo1gPU";
+const GAMEID = "DVaVfdLo2lw1Nv9dZDOr";
 const refreshBtn = document.getElementById('refresh');
-refreshBtn.addEventListener('click', refreshScores);
+refreshBtn.addEventListener('click', () => refreshScores(GAMEID));
 
 const form = document.querySelector('.score-form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  addScore(form.playerName.value, form.playerScore.value);
+  addScore(GAMEID, form.playerName.value, form.playerScore.value);
+  form.playerName.value = "";
+  form.playerScore.value = "";
 });
-
-// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/5SLb2yuqigv9cgRo1gPU/scores/', {
-//   method: 'POST',
-//   body: JSON.stringify({
-//     user: "Test",
-//     score: 30
-//   }),
-//   headers: {
-//     'Content-type': 'application/json; charset=UTF-8',
-//   },
-// })
-//   .then((response) => response.json())
-//   .then((json) => console.log(json))
-//   .catch((err) => console.log(err));
+refreshScores(GAMEID);
