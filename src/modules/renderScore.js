fb98b911e833bc0b67aa2d9bbeccd64ed7463b77
shubham <shubham.bhoyar077@gmail.com>
2023-03-07 20:14:29 +0530

    Sorting list

--- src/modules/renderScore.js
+++ src/modules/renderScore.js
@@ -1,6 +1,7 @@
 const renderScore = (scoreRecords) => {
   const ulList = document.getElementById('scores-list');
   ulList.innerHTML = "";
+  scoreRecords.sort((a, b) => b.score - a.score);
   scoreRecords.forEach(element => {
     ulList.innerHTML += `
         <li class="score">${element.user}: ${element.score}</li>`;
