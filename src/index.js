58f7dcb717993ff732a1834a2d3bf179e51a2949
shubham <shubham.bhoyar077@gmail.com>
2023-03-07 19:29:31 +0530

    Remov hardcoded array and calling get using refresh button

--- src/index.js
+++ src/index.js
@@ -1,38 +1,26 @@
 import './css/style.css';
-import renderScore from './modules/renderScore.js';
 import addScore from './modules/addScore.js';
+import refreshScores from './modules/getScores.js';
 
-const playerScore = [
-  {
-    name: 'Example1',
-    score: '100',
-  },
-  {
-    name: 'Example2',
-    score: '20',
-  },
-  {
-    name: 'Example3',
-    score: '50',
-  },
-  {
-    name: 'Example4',
-    score: '10',
-  },
-  {
-    name: 'Example5',
-    score: '60',
-  },
-  {
-    name: 'Example6',
-    score: '30',
-  },
-];
-
-playerScore.forEach((element) => renderScore(element));
+const refreshBtn = document.getElementById('refresh');
+refreshBtn.addEventListener('click', refreshScores);
 
 const form = document.querySelector('.score-form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   addScore(form.playerName.value, form.playerScore.value);
 });
+
+// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/5SLb2yuqigv9cgRo1gPU/scores/', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     user: "Test",
+//     score: 30
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json))
+//   .catch((err) => console.log(err));
