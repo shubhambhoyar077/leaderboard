bcaa0f51214e959914de23d32ea2c3fe44219a54
shubham <shubham.bhoyar077@gmail.com>
2023-03-08 14:53:17 +0530

    Add scrolling and improve ui for submit button

--- src/index.js
+++ src/index.js
@@ -2,20 +2,26 @@ import './css/style.css';
 import addScore from './modules/addScore.js';
 import refreshScores from './modules/getScores.js';
 
+const TESTGAMEID = '5SLb2yuqigv9cgRo1gPU';
 const GAMEID = 'DVaVfdLo2lw1Nv9dZDOr';
 const refreshBtn = document.getElementById('refresh');
-refreshBtn.addEventListener('click', () => refreshScores(GAMEID));
+refreshBtn.addEventListener('click', () => refreshScores(TESTGAMEID));
 
 const form = document.querySelector('.score-form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  addScore(GAMEID, form.playerName.value, form.playerScore.value)
+  addScore(TESTGAMEID, form.playerName.value, form.playerScore.value)
     .then((data) => {
-      // This console log is use for test.
-      // It will be replace in next PR when I work on styling.
-      console.log(data);
+      form.submit.innerHTML = `Submit`
+      form.submit.disabled = false; 
+    })
+    .catch((err) => {
+      form.submit.innerHTML = `Submit`
+      form.submit.disabled = false; 
     });
+  form.submit.innerHTML = `Adding... <i class="fa fa-spinner fa-spin"></i>`;
+  form.submit.disabled = true; 
   form.playerName.value = '';
   form.playerScore.value = '';
 });
-refreshScores(GAMEID);
+refreshScores(TESTGAMEID);
