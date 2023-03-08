e053c087e9aa9776255ba3efce47883ea49fead6
shubham <shubham.bhoyar077@gmail.com>
2023-03-08 14:02:04 +0530

    Add crown icon to top player

--- src/modules/renderScore.js
+++ src/modules/renderScore.js
@@ -2,9 +2,15 @@ const renderScore = (scoreRecords) => {
   const ulList = document.getElementById('scores-list');
   ulList.innerHTML = '';
   scoreRecords.sort((a, b) => b.score - a.score);
-  scoreRecords.forEach((element) => {
-    ulList.innerHTML += `
+  scoreRecords.forEach((element, index) => {
+    if(index === 0){
+      ulList.innerHTML += `
+        <li class="score">${element.user}: ${element.score} <i class='fas fa-crown'></i></li>`;
+    }else{
+      ulList.innerHTML += `
         <li class="score">${element.user}: ${element.score}</li>`;
+    }
+    
   });
 };
 
